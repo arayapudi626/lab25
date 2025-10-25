e3694b14601ea86951a90d6e2f95b785e7d1b9c3
arayapudi626 <rayapudianisha@gmail.com>
2025-10-25 12:39:15 -0700

    update

--- lab25.cpp
+++ lab25.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
 #include <chrono>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <list>
+#include <set>
 using namespace std;
+using namespace std::chrono;
 
 int main() {
+    //reading
+    string line;
+    auto start = high_resolution_clock::now();
+    ifstream fin("codes.txt");
+   while 
+    (getline(fin, line));{
+       cout << line << endl;
+    }
+    auto end = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(end-start);
+    cout << "TIME: " << duration.count();
     
+
+    //output
+
+
 
     return 0;
 }
